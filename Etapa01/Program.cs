f89ceb659459605a7fc6881b451c7e0d9749d9a6
Emerson <emersonfonseca@aluno.uespi.br>
2023-11-22 14:13:03 -0300

    Calculadora

--- Etapa01/Program.cs
+++ Etapa01/Program.cs
@@ -1,13 +1,52 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 
-public class Program
+class Program
 {
-    public static void Main(string[] args)
+    static void Main()
     {
-    string text = "coding";
-    //text = "coding is hard";
-    text = "coding is fun";
-    Console.WriteLine(text);
+        double num1, num2, resultado;
+        string operador;
+        
+        Console.Write("Digite o primeiro número: ");
+        num1 = Convert.ToDouble(Console.ReadLine());
+       
+        Console.Write("Digite o operador (+, -, *, /): ");
+        
+        operador = Console.ReadLine();
+
+        Console.Write("Digite o segundo número: ");
+        num2 = Convert.ToDouble(Console.ReadLine());
+
+        switch (operador)
+        {
+            case "+":
+                resultado = num1 + num2;
+                Console.WriteLine("Resultado: " + resultado);
+                break;
+            case "-":
+                resultado = num1 - num2;
+                Console.WriteLine("Resultado: " + resultado);
+                break;
+            case "*":
+                resultado = num1 * num2;
+                Console.WriteLine("Resultado: " + resultado);
+                break;
+            case "/":
+                if (num2 != 0)
+                {
+                    resultado = num1 / num2;
+                    Console.WriteLine("Resultado: " + resultado);
+                }
+                else
+                {
+                    Console.WriteLine("Erro! Divisão por zero não é permitida.");
+                }
+                break;
+            default:
+                Console.WriteLine("Operador inválido.");
+                break;
+        }
     }
 }
+
--- Etapa01/obj/Debug/net8.0/Etapa01.AssemblyInfo.cs
+++ Etapa01/obj/Debug/net8.0/Etapa01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Etapa01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a31f81a3f852e067512784b7a070de276037db5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Etapa01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Etapa01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
